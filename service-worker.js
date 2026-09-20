6f24a1c002a4f5ca90430e462e3684b32c9c5ff4
Nader-Elwarsh <nadermalak92@gmail.com>
2026-09-20 12:28:54 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "workshop-v11-115-route-actions-fix";
+const CACHE_NAME = "workshop-v11-117-auto-backup";
 importScripts("./notif-shared.js");
 importScripts("./share-store.js");
 const CORE_FILES = [
